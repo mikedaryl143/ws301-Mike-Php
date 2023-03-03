95669e06298b867de27a8f7b93bf0889fef6f3e0
mikedaryl143 <mikemangila28@gmail.com>
2023-03-03 14:53:38 +0800

    First Commit

---
+++ pages/blank.php
@@ -0,0 +1,13 @@
+<?php 
+include ("../templates/header.php");
+?>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">Blank</h1>
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <?php 
+include ("../templates/footer.php");
+?>              
---
+++ templates/footer.php
@@ -0,0 +1,53 @@
+</div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- jQuery -->
+        <script src="../js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="../js/metisMenu.min.js"></script>
+
+        <!-- Morris Charts JavaScript -->
+        <script src="../js/raphael.min.js"></script>
+        <script src="../js/morris.min.js"></script>
+        <script src="../js/morris-data.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="../js/startmin.js"></script>
+        
+          <!-- DataTables JavaScript -->
+          <script src="../js/dataTables/jquery.dataTables.min.js"></script>
+        <script src="../js/dataTables/dataTables.bootstrap.min.js"></script>
+
+        <!-- Flot Charts JavaScript -->
+        <script src="../js/flot/excanvas.min.js"></script>
+        <script src="../js/flot/jquery.flot.js"></script>
+        <script src="../js/flot/jquery.flot.pie.js"></script>
+        <script src="../js/flot/jquery.flot.resize.js"></script>
+        <script src="../js/flot/jquery.flot.time.js"></script>
+        <script src="../js/flot/jquery.flot.tooltip.min.js"></script>
+        <script src="../js/flot-data.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="../js/startmin.js"></script>
+
+        <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+        <script>
+        
+        $(document).ready(function() {
+                $('#dataTables-example').DataTable({
+                        responsive: true
+                });
+            });
+        </script>
+
+    </body>
+</html>
